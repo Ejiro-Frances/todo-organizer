71db5bb7a3dd16805c0bef41347f63f8f823b9d2
Ejiro-Frances <francesejiro@yahoo.com>
2025-06-16 07:31:52 +0100

    adjusted config

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import "./styles.css";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
-import "./styles.css";
 const queryClient = new QueryClient();
 // Create a new router instance
 const router = createRouter({
